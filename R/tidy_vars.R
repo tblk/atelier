fd7b3966f67fabf61f9625997861307fa5dbd7d8
tblk <theo.boulakia@orange.fr>
2026-03-18 16:13:08 +0100

    simplify tidy vars

---
+++ R/tidy_vars.R
@@ -0,0 +1,19 @@
+#' tidy MCA infos on vars
+#'
+#' @param x Le résultat de FactoMineR::MCA
+#' @return Un data.frame avec toutes les informations sur les
+#' variables/modalités actives
+#' @export
+tidy_vars = function(x) {
+
+  x$var[1:4] |>
+    purrr::map(~as.data.frame(.x) |>
+                 tibble::rownames_to_column(var = "variable_modalite")) |>
+    purrr::list_rbind(names_to = "indicateur") |>
+    dplyr::rename_with(~stringr::str_replace(.x, " ", "")) |>
+    dplyr::rename_with(tolower) |>
+    tidyr::pivot_wider(names_from = .data$indicateur,
+                       values_from = dplyr::starts_with("dim"),
+                       names_sep = "_")
+
+}
