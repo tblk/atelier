2847528c021a0d9d9c60f07b896dc968417ad594
tblk <theo.boulakia@orange.fr>
2026-03-18 15:33:29 +0100

    clean useless acm functions

--- R/rare_cats.R
+++ R/rare_cats.R
@@ -5,46 +5,23 @@
 #'
 #' @param data data.frame de variables qualitatives
 #' @param min_pct seuil minimal de fréquence (%)
-#' @param detect_na exclure les modalités correspondant à NA recodés
-#' @param na_pattern regex identifiant les NA recodés
-#'
 #' @return Un vecteur de modalités à exclure
 #' @export
-rare_cats <- function(
-    data,
-    min_pct = 5,
-    detect_na = TRUE,
-    na_pattern = "\\sNA$"
-) {
+rare_cats = function(data, min_pct = 5) {
 
-  # ---- fréquences ----
-  freq_tbl <-
-    purrr::map(names(data), function(var_name) {
-      tibble::tibble(modalite = data[[var_name]]) |>
-        dplyr::count(.data$modalite, name = "effectif") |>
-        dplyr::mutate(
-          pourcentage = 100 * .data$effectif / sum(.data$effectif),
-          variable = var_name
-        )
-    }) |>
+  freq_tbl = purrr::map(names(data), function(var_name) {
+    tibble::tibble(modalite = data[[var_name]]) |>
+      dplyr::count(.data$modalite, name = "effectif") |>
+      dplyr::mutate(
+        pourcentage = 100 * .data$effectif / sum(.data$effectif),
+        variable = var_name
+      )
+  }) |>
     purrr::list_rbind()
 
-  # ---- condition rareté ----
-  rare_cond <- freq_tbl$pourcentage < min_pct
-
-  # ---- condition NA robuste ----
-  if (detect_na) {
-    na_cond <- stringr::str_detect(freq_tbl$modalite, na_pattern)
-  } else {
-    na_cond <- FALSE
-  }
-
-  # ---- modalités exclues ----
-  to_exclude <-
-    freq_tbl |>
-    dplyr::filter(rare_cond | na_cond) |>
+  freq_tbl |>
+    dplyr::filter(stringr::str_detect(.data$modalite, "\\sNA$") |
+                    .data$pourcentage < min_pct) |>
     dplyr::pull(.data$modalite) |>
     unique()
-
-  return(to_exclude)
 }
