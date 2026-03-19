fd20099e7187dfb7780753b6627db4d5bfcb3f39
tblk <theo.boulakia@orange.fr>
2026-03-19 12:54:08 +0100

    add get pcs coord

---
+++ R/get_pcs_coord.R
@@ -0,0 +1,19 @@
+#' Title Get PCS coord after dimension reduction
+#'
+#' @param data a dataframe with dimensions and pcs grouping variables
+#' @param var a pcs niv
+#'
+#' @returns a summarised dataframe
+#' @export
+#'
+get_pcs_coord = function(data, var) {
+  data |>
+    dplyr::filter(!is.na(.data[[var]])) |>
+    dplyr::summarise(
+      dplyr::across(
+        dplyr::starts_with("dim"),
+        mean),
+      .by = dplyr::all_of(var)) |>
+    dplyr::rename(pcs_code = dplyr::all_of(var)) |>
+    dplyr::mutate(pcs_code = as.character(.data$pcs_code))
+}
